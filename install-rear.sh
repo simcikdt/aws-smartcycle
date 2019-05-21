851f3d1a1b438514fca64142d90992449d405884
simcik <simcik@gmail.com>
2019-05-21 12:30:55 -0400

    Removed pip installed ant package as it won't work, need Git installed version instead
    Updated the install shell scripts to pull from Bader's GitHub repo for the correct ANT Python module. Was running into USBDrive2 missing module errors with the default Ant package from pip. Bader's version has the correct USB2 drive.

--- install-rear.sh
+++ install-rear.sh
@@ -2,14 +2,25 @@
 
 echo "Hello World!"
 
+apt-get -y install vim
+
 pip install --upgrade pip
 
 hash python2  2>/dev/null || { echo >&2 "I require Python2 but it's not installed.  Aborting."; exit 1; }
-hash pip 2>/dev/null || { echo >&2 "I require Pip but it's not installed.  Aborting."; exit 1; }
+hash pip2 2>/dev/null || { echo >&2 "I require Pip but it's not installed.  Aborting."; exit 1; }
 
 pip2 install -r /home/aws_cam/aws-smartcycle/sensors/requirements.txt;
 pip2 install -r /home/aws_cam/aws-smartcycle/audio/requirements.txt;
 
-dmesg | tail
+cd ..
+git clone https://github.com/baderj/python-ant.git
+
+apt-get install -y python-setuptools
+cd python-ant/
+python setup.py install
 
 cp /home/aws_cam/aws-smartcycle/sensors/garmin-ant2.rules /etc/udev/rules.d/garmin-ant2.rules
+
+lsusb
+
+dmesg | tail
