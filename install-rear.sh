9f18fb4f6aed71081121990c042bd8a00791ca63
simcik <simcik@gmail.com>
2019-05-21 11:20:33 -0400

    Hardcoded aws_cam user directory home into pip install paths

--- install-rear.sh
+++ install-rear.sh
@@ -5,5 +5,5 @@ echo "Hello World!"
 hash python2  2>/dev/null || { echo >&2 "I require Python2 but it's not installed.  Aborting."; exit 1; }
 hash pip 2>/dev/null || { echo >&2 "I require Pip but it's not installed.  Aborting."; exit 1; }
 
-pip install -r ~/aws-smartcycle/sensors/requirements.txt;
-pip install -r ~/aws-smartcycle/audio/requirements.txt;
+pip install -r /home/aws_cam/aws-smartcycle/sensors/requirements.txt;
+pip install -r /home/aws_cam/aws-smartcycle/audio/requirements.txt;
