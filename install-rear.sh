e18807c49575f5213ef9f6c77e2b57a0fa938ad0
simcik <simcik@gmail.com>
2019-05-21 11:30:30 -0400

    Switched to pip2, perform pip upgrade before installing any Python packages

--- install-rear.sh
+++ install-rear.sh
@@ -2,8 +2,10 @@
 
 echo "Hello World!"
 
+pip install --upgrade pip
+
 hash python2  2>/dev/null || { echo >&2 "I require Python2 but it's not installed.  Aborting."; exit 1; }
 hash pip 2>/dev/null || { echo >&2 "I require Pip but it's not installed.  Aborting."; exit 1; }
 
-pip install -r /home/aws_cam/aws-smartcycle/sensors/requirements.txt;
-pip install -r /home/aws_cam/aws-smartcycle/audio/requirements.txt;
+pip2 install -r /home/aws_cam/aws-smartcycle/sensors/requirements.txt;
+pip2 install -r /home/aws_cam/aws-smartcycle/audio/requirements.txt;
